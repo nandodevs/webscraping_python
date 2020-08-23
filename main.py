a469559787bad90b375e802cc8292e6d297f579d
Sisnando Junior <nando.devs@gmail.com>
2020-08-23 17:55:13 -0300

    Projeto Atualizado

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
-res = requests.get('https://www.filmesonlinegratisbr.com/genero/lancamentos/')
+res = requests.get('http://gofilmes.band/genero/lancamentos')
 res.encoding = 'utf-8'
 
 soup = BeautifulSoup(res.text, 'html.parser')
+posts = soup.find_all(class_='poster')
 
+all_posts = []
+for post in posts:
+    info = post.find(class_='poster')
+    ano = info.find(class_='t-ano').text
+    title = info.find(class_='t-tit').text
+    time = info.find(class_='t-tempo').text
+    audio = info.find(class_='t-audio').text
+    img = info.find(class_='thumb')['data-src']
 
-all_posts = soup.find_all(class_='box')
+    all_posts.append(
+        {'titulo': title,
+         'ano': ano,
+         'img': img,
+         'tempo': time,
+         'audio': audio
+         })
 
 print(all_posts)
+with open('posts.json', 'w') as json_file:
+    json.dump(all_posts, json_file, indent=3, ensure_ascii=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
